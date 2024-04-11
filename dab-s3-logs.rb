c9ca1a25d86700546d31794402bcd8e63ca4d55f
NickMoignard <nicklmoignard@gmail.com>
2024-04-11 16:10:11 +1000

    another fix for release URL

--- dab-s3-logs.rb
+++ dab-s3-logs.rb
@@ -1,7 +1,7 @@
 class DabS3Logs < Formula
   desc "Small utility to download and parse logs from S3"
   homepage "https://github.com/NickMoignard/dab-s3-logs"
-  url "https://github.com/NickMoignard/dab-s3-logs/releases/download/v0.1.3-alpha/dab-s3-logs.tar.gz"
+  url "https://github.com/NickMoignard/dab-s3-logs/releases/download/v0.1.3-alpha/dab-s3-logs-aarch64-apple-darwin.tar.gz"
   sha256 "645d134e71f87b7a20fea76ae11b4e7192a4e60ecb92b70c5b1c6d1d414cab2d"
   license "MIT"
 
