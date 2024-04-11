195035bddd943d9e498d438d36cc4e838c322fb7
NickMoignard <nicklmoignard@gmail.com>
2024-04-11 16:04:07 +1000

    release dab-s3-logs v0.1.3-alpha

--- dab-s3-logs.rb
+++ dab-s3-logs.rb
@@ -1,8 +1,8 @@
 class DabS3Logs < Formula
   desc "Small utility to download and parse logs from S3"
   homepage "https://github.com/NickMoignard/dab-s3-logs"
-  url "https://github.com/NickMoignard/d3-tui/releases/download/v0.1.1-alpha/dab-s3-logs.tar.gz"
-  sha256 "bc2a9ea9916d13cbe0868ab3982dff8bbe99388113e75c634856939afe52c400"
+  url "https://github.com/NickMoignard/d3-tui/releases/download/v0.1.3-alpha/dab-s3-logs.tar.gz"
+  sha256 "645d134e71f87b7a20fea76ae11b4e7192a4e60ecb92b70c5b1c6d1d414cab2d"
   license "MIT"
 
   def install
